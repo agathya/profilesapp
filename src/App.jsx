dae031afd907ed82c58c4f59e1414fe8d3264453
agathya <39418478+agathya@users.noreply.github.com>
2024-12-14 11:56:58 -0500

    dec-14

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to prabhu HMR
+          Edit <code>src/App.jsx</code> and save to tested new HMR
         </p>
       </div>
       <p className="read-the-docs">
